5911757f7efed12b146608842f21fded3a274da2
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-13 16:11:20 +0100

    fatta funzione per gestire i campi del form e implementata chiamata POST con axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  });
+
+  function handleFormData(e) {
+    const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+    setFormData({
+      ...formData,
+      [e.target.name]: value,
+    });
+  }
+
+  function chiamataAxios() {
+    axios.post(
+      "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts",
+      formData
+    )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  }
 
 
 
@@ -10,6 +38,6 @@ function App() {
 
     </>
   )
-}
 
+}
 export default App
